81b53b0883e2940c813c1f9f181fc4536e22780d
mocha-matcha <sana.nacnac@gmail.com>
2022-07-03 22:35:06 -0500

    theme adventures. connect css to javascript ahaha.s

--- components/HeaderBar.vue
+++ components/HeaderBar.vue
@@ -1,30 +1,23 @@
 <template>
-    <header class="header">
-
-        <div class="container">
-
-                <h1>Cute-Blog</h1>
-
-                <div class="links">
-                    <NuxtLink to="/"> Home </NuxtLink>
-                    <NuxtLink to="/blog"> Blog </NuxtLink>
-                    <NuxtLink to="/project"> Projects </NuxtLink>
-                    <NuxtLink to="/about"> About </NuxtLink>
-            </div>
-        </div>
-
-
-    </header>
-
+  <header class="header">
+    <div class="container">
+      <div class="links">
+        <NuxtLink to="/"> Home </NuxtLink>
+        <NuxtLink to="/blog"> Blog </NuxtLink>
+        <NuxtLink to="/project"> Projects </NuxtLink>
+        <NuxtLink to="/about"> About </NuxtLink>
+      </div>
+    </div>
+  </header>
 </template>
 
-
-
 <style>
+
 .links {
-    font-size: large;
-    font-style: italic;
-    font-weight: bold;
-    text-align: right;
+  font-size: large;
+  font-style: italic;
+  font-weight: bold;
+  text-align: right;
 }
+
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,48 +3,99 @@
   <NuxtPage></NuxtPage>
 </template>
 
-
-
-<script>
-const d = new Date()
-let currentMonth = d.getMonth()
-console.log(currentMonth)
-
-// Dec Jan Feb = winter
+<!-- // Dec Jan Feb = winter
 // March April May = Spring
 // June July August = Summer
 // Sep Oct Nov = Fall
 
 
-
-
-
-</script>
-
-<!-- Change colors based on the seasons -->
-<!-- green for spring
-yellow for summer
-blue for winter
-brown for fall -->
-<style>
+/* Spring */
 /* MintyGreen:#AEEBB2 */
 /* OliveGreen:#6D9277 */
 
+/* Summer */
+/* Lemon:#FBFFB1  */
+/* Peach:#F3B657  */
+
+/* Winter */
+/* Ice:#C9FBFF */
+/* Navy:#142161 */
+
+/* Fall */
+/* Brown:#613014 */
+/* Huh:#ECD7A1  */ -->
+<script setup>
+const d = new Date();
+let currentMonth = d.getMonth();
+console.log(currentMonth);
+const winterMonths = [12, 1, 2];
+const springMonths = [3, 4, 5];
+const summerMonths = [6, 7, 8];
+const fallMonths = [9, 10, 11];
+const winterTheme = {
+  baseColor: '#C9FBFF',
+  accentColor: '#142161'
+};
+const springTheme = {
+  baseColor: '#6D9277',
+  accentColor: '#AEEBB2'
+};
+const summerTheme = {
+  baseColor: '--peach',
+  accentColor: '--lemon'
+};
+const fallTheme = {
+  baseColor: '#613014',
+  accentColor: '#ECD7A1'
+};
+const months = [winterMonths, springMonths, summerMonths, fallMonths];
+const themes = [winterTheme,springTheme,summerTheme,fallTheme];
+const currentTheme = summerTheme
+console.log(currentTheme.baseColor)
+console.log(currentTheme.accentColor)
+
+
+
+
+
+// console.log(currentTheme.baseColor)
+</script>
+
+
+
+
+<style scoped>
 :root {
   --oliveGreen: #6d9277;
   --mintyGreen: #aeebb2;
-  --baseColor: var(--oliveGreen);
-  --accentColor: var(--mintyGreen);
+  --lemon:#FBFFB1 ;
+--peach:#F3B657;
+
+--ice:#C9FBFF;
+--navy:#142161;
+
+/* Fall */
+--brown:#613014;
+--huh:#ECD7A1 ;
+
+  /* --baseColor: var(--oliveGreen);
+  --accentColor: var(--mintyGreen); */
+  --baseColor: var(v-bind('currentTheme.baseColor'));
+  --accentColor: var(v-bind('currentTheme.accentColor'));
   font-family: "Courier New", Courier, monospace;
+
   background-color: var(--baseColor);
   color: var(--accentColor);
 }
 
 .container {
+  border-radius: 10px;
   align-self: center;
-  text-align: justify;
+  text-align: center;
   background-color: var(--accentColor);
   color: var(--baseColor);
   margin: auto;
+  width: auto;
+  height: auto;
 }
 </style>
